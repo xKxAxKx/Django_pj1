aeef30a69a2f51278b030b79832e133081d637bd
xKxAxKx <pekepooooong@gmail.com>
2017-01-15 19:09:53 +0900

    edit dbsetting

--- pj1/settings.py
+++ pj1/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'pj1.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'app1_db',
+        'USER': 'app1_user',
+        'PASSWORD': 'app1_passwd',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
